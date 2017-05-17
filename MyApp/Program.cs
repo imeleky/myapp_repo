6093ec37df740837cef5ff726ccbd323c2fc47e2
imy <ilkaymy@yahoo.com>
2017-05-17 18:48:35 +0300

    first commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
